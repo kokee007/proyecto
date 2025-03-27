658c11c7c7c0add1d8b49c996eb122ec05dcdbb9
Alan <alantronchi@ceroca.cat>
2025-03-27 17:04:55 +0100

    comentari per arreglar

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,//hoal
       // Para que el usuario primero se autentique:
       initialRoute: '/login',
       routes: {
